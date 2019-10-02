d050a087f3324a1f73b8bb2c4b41d112034b9b1c
rhexy ilham madany <rehexy_173040125@mail.unpas.ac.id>
2019-10-02 16:55:13 +0700

    menambahkan folder autoloading dan di dalamnya terdapat file php tujuan nya untuk memudahkan requir

---
+++ autoloading/App/init.php
@@ -0,0 +1,11 @@
+<?php 
+
+// require_once 'Produk/InfoProduk.php';
+// require_once 'Produk/produk.php';
+// require_once 'Produk/komik.php';
+// require_once 'Produk/Game.php';
+// require_once 'Produk/CetakInfoProduk.php';
+
+spl_autoload_register(function($class){
+	require_once __DIR__ . '/Produk/' .$class.'.php';
+});
